5225c803cd4738c0bb4cb6f821e3bec351bfa1cd
Hossein Tayebi <htayebi@gmail.com>
2024-12-11 12:05:01 +0000

    adding stuff to General

--- app/frontend/src/pages/general/General.tsx
+++ app/frontend/src/pages/general/General.tsx
@@ -1,11 +1,151 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import { Panel, DefaultButton } from "@fluentui/react";
+import { Settings } from "../../components/Settings/Settings";
+import { useMsal } from "@azure/msal-react";
+import { useLogin, getToken, requireAccessControl } from "../../authConfig";
+import { askApi, configApi, ChatAppResponse, ChatAppRequest, RetrievalMode, VectorFieldOptions, GPT4VInput, SpeechConfig } from "../../api";
+import { LoginContext } from "../../loginContext";
+import { useTranslation } from "react-i18next";
+import { TokenClaimsDisplay } from "../../components/TokenClaimsDisplay";
 
-const General = () => {
+export function Component(): JSX.Element {
+    const [isConfigPanelOpen, setIsConfigPanelOpen] = useState(false);
+    const [promptTemplate, setPromptTemplate] = useState<string>("");
+    const [promptTemplatePrefix, setPromptTemplatePrefix] = useState<string>("");
+    const [promptTemplateSuffix, setPromptTemplateSuffix] = useState<string>("");
+    const [temperature, setTemperature] = useState<number>(0.3);
+    const [seed, setSeed] = useState<number | null>(null);
+    const [minimumRerankerScore, setMinimumRerankerScore] = useState<number>(0);
+    const [minimumSearchScore, setMinimumSearchScore] = useState<number>(0);
+    const [retrievalMode, setRetrievalMode] = useState<RetrievalMode>(RetrievalMode.Hybrid);
+    const [retrieveCount, setRetrieveCount] = useState<number>(3);
+    const [useSemanticRanker, setUseSemanticRanker] = useState<boolean>(true);
+    const [useSemanticCaptions, setUseSemanticCaptions] = useState<boolean>(false);
+    const [useGPT4V, setUseGPT4V] = useState<boolean>(false);
+    const [gpt4vInput, setGPT4VInput] = useState<GPT4VInput>(GPT4VInput.TextAndImages);
+    const [includeCategory, setIncludeCategory] = useState<string>("");
+    const [excludeCategory, setExcludeCategory] = useState<string>("");
+    const [question, setQuestion] = useState<string>("");
+    const [vectorFieldList, setVectorFieldList] = useState<VectorFieldOptions[]>([VectorFieldOptions.Embedding, VectorFieldOptions.ImageEmbedding]);
+    const [useOidSecurityFilter, setUseOidSecurityFilter] = useState<boolean>(false);
+    const [useGroupsSecurityFilter, setUseGroupsSecurityFilter] = useState<boolean>(false);
+    const [showGPT4VOptions, setShowGPT4VOptions] = useState<boolean>(false);
+    const [showSemanticRankerOption, setShowSemanticRankerOption] = useState<boolean>(false);
+    const [showVectorOption, setShowVectorOption] = useState<boolean>(false);
+    const [showUserUpload, setShowUserUpload] = useState<boolean>(false);
+    const [showLanguagePicker, setshowLanguagePicker] = useState<boolean>(false);
+    const [showSpeechInput, setShowSpeechInput] = useState<boolean>(false);
+    const [showSpeechOutputBrowser, setShowSpeechOutputBrowser] = useState<boolean>(false);
+    const [showSpeechOutputAzure, setShowSpeechOutputAzure] = useState<boolean>(false);
+    const audio = useRef(new Audio()).current;
+    const [isPlaying, setIsPlaying] = useState(false);
+    const client = useLogin ? useMsal().instance : undefined;
+    const { loggedIn } = useContext(LoginContext);
+    const { t, i18n } = useTranslation();
+
+    const handleSettingsChange = (field: string, value: any) => {
+        switch (field) {
+            case "promptTemplate":
+                setPromptTemplate(value);
+                break;
+            case "promptTemplatePrefix":
+                setPromptTemplatePrefix(value);
+                break;
+            case "promptTemplateSuffix":
+                setPromptTemplateSuffix(value);
+                break;
+            case "temperature":
+                setTemperature(value);
+                break;
+            case "seed":
+                setSeed(value);
+                break;
+            case "minimumRerankerScore":
+                setMinimumRerankerScore(value);
+                break;
+            case "minimumSearchScore":
+                setMinimumSearchScore(value);
+                break;
+            case "retrieveCount":
+                setRetrieveCount(value);
+                break;
+            case "useSemanticRanker":
+                setUseSemanticRanker(value);
+                break;
+            case "useSemanticCaptions":
+                setUseSemanticCaptions(value);
+                break;
+            case "excludeCategory":
+                setExcludeCategory(value);
+                break;
+            case "includeCategory":
+                setIncludeCategory(value);
+                break;
+            case "useOidSecurityFilter":
+                setUseOidSecurityFilter(value);
+                break;
+            case "useGroupsSecurityFilter":
+                setUseGroupsSecurityFilter(value);
+                break;
+            case "useGPT4V":
+                setUseGPT4V(value);
+                break;
+            case "gpt4vInput":
+                setGPT4VInput(value);
+                break;
+            case "vectorFieldList":
+                setVectorFieldList(value);
+                break;
+            case "retrievalMode":
+                setRetrievalMode(value);
+                break;
+        }
+    };
 
     return (
         <div>
+            <Panel
+                headerText={t("labels.headerText")}
+                isOpen={isConfigPanelOpen}
+                isBlocking={false}
+                onDismiss={() => setIsConfigPanelOpen(false)}
+                closeButtonAriaLabel={t("labels.closeButton")}
+                onRenderFooterContent={() => <DefaultButton onClick={() => setIsConfigPanelOpen(false)}>{t("labels.closeButton")}</DefaultButton>}
+                isFooterAtBottom={true}
+            >
+                <Settings
+                    promptTemplate={promptTemplate}
+                    promptTemplatePrefix={promptTemplatePrefix}
+                    promptTemplateSuffix={promptTemplateSuffix}
+                    temperature={temperature}
+                    retrieveCount={retrieveCount}
+                    seed={seed}
+                    minimumSearchScore={minimumSearchScore}
+                    minimumRerankerScore={minimumRerankerScore}
+                    useSemanticRanker={useSemanticRanker}
+                    useSemanticCaptions={useSemanticCaptions}
+                    excludeCategory={excludeCategory}
+                    includeCategory={includeCategory}
+                    retrievalMode={retrievalMode}
+                    useGPT4V={useGPT4V}
+                    gpt4vInput={gpt4vInput}
+                    vectorFieldList={vectorFieldList}
+                    showSemanticRankerOption={showSemanticRankerOption}
+                    showGPT4VOptions={showGPT4VOptions}
+                    showVectorOption={showVectorOption}
+                    useOidSecurityFilter={useOidSecurityFilter}
+                    useGroupsSecurityFilter={useGroupsSecurityFilter}
+                    useLogin={!!useLogin}
+                    loggedIn={loggedIn}
+                    requireAccessControl={requireAccessControl}
+                    onChange={handleSettingsChange}
+                />
+                {useLogin && <TokenClaimsDisplay />}
+            </Panel>
+
             <h1>Hello testing</h1>
         </div>
     );
-};
+}
 
-export default General;
+Component.displayName = "General";
